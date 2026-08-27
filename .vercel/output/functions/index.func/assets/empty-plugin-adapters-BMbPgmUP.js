ededce34ea7326734c154f4608327e5aae2e7351
sarangs1621 <sarangsnair1621@gmail.com>
2026-08-27 17:59:36 +0530

    chore: ignore and remove local .vercel output directory

--- .vercel/output/functions/index.func/assets/empty-plugin-adapters-BMbPgmUP.js
+++
@@ -1,5 +0,0 @@
-//#region node_modules/.bun/@tanstack+start-server-core@1.169.16+be8a7a04c56ced13/node_modules/@tanstack/start-server-core/dist/esm/empty-plugin-adapters.js
-var pluginSerializationAdapters = [];
-var hasPluginAdapters = false;
-//#endregion
-export { hasPluginAdapters, pluginSerializationAdapters };
