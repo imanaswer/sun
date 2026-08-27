bdf45604e0cef2baa013fcf40f2b12c3e5be16da
sarangs1621 <sarangsnair1621@gmail.com>
2026-08-27 21:06:59 +0530

    fix: ensure ScrollArea scrollbar is visible on mobile

--- src/components/umberlla/sections.tsx
+++ src/components/umberlla/sections.tsx
@@ -330,7 +330,7 @@ export function SiteNav() {
           &times;
         </button>
       </div>
-      <ScrollArea className="flex-1 min-h-0">
+      <ScrollArea type="scroll" className="flex-1 min-h-0">
         <nav aria-label="Categories" className="px-6 pb-24">
         {NAV_LINKS.map((l, i) => (
           <div
